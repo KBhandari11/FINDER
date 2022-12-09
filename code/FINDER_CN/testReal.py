78d347827df85220077d59cb94ad754020dfc51e
KBhandari11 <32970056+KBhandari11@users.noreply.github.com>
2022-12-09 17:37:59 -0500

    Update testReal for colab

--- code/FINDER_CN/testReal.py
+++ code/FINDER_CN/testReal.py
@@ -1,26 +1,39 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import sys,os
+sys.path.append(os.path.dirname(__file__) + os.sep + '../')
 from FINDER import FINDER
 import numpy as np
+from tqdm import tqdm
 import time
+import networkx as nx
 import pandas as pd
+import pickle as cp
+import graph
+import csv
+import numpy as  np 
+import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
 
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 def GetSolution(STEPRATIO, MODEL_FILE_CKPT):
     ######################################################################################################################
     ##................................................Get Solution (model).....................................................
     dqn = FINDER()
-    data_test_path = '../data/real/'
-#     data_test_name = ['Crime','HI-II-14','Digg','Enron','Gnutella31','Epinions','Facebook','Youtube','Flickr']
-    data_test_name = ['Crime','HI-II-14']
-    model_file_path = './FINDER_CN/models/'
-    model_file_ckpt = MODEL_FILE_CKPT
-    model_file = model_file_path + model_file_ckpt
+    data_test_path = '/content/FINDER/code/FINDER_CN/data/real/'
+    #data_test_name = ['Crime','HI-II-14','Digg','Enron','Gnutella31','Epinions','Facebook','Youtube','Flickr']
+    data_test_name = [f for f in listdir(data_test_path) if isfile(join(data_test_path, f))]
+    #model_file_path = '/content/FINDER/code/FINDER_CN/models/barabasi_albert/'
+    #model_file_ckpt = MODEL_FILE_CKPT
+    model_file = MODEL_FILE_CKPT
     ## save_dir
-    save_dir = '../results/FINDER_CN/real'
+    save_dir = '/content/FINDER/code/results/'
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
+        
     ## begin computing...
     print ('The best model is :%s'%(model_file))
     dqn.LoadModel(model_file)
@@ -29,7 +42,7 @@ def GetSolution(STEPRATIO, MODEL_FILE_CKPT):
     stepRatio = STEPRATIO
     for j in range(len(data_test_name)):
         print ('\nTesting dataset %s'%data_test_name[j])
-        data_test = data_test_path + data_test_name[j] + '.txt'
+        data_test = data_test_path + data_test_name[j] 
         solution, time = dqn.EvaluateRealData(model_file, data_test, save_dir, stepRatio)
         df.iloc[0,j] = time
         print('Data:%s, time:%.2f'%(data_test_name[j], time))
@@ -43,16 +56,16 @@ def EvaluateSolution(STEPRATIO, MODEL_FILE_CKPT, STRTEGYID):
     #######################################################################################################################
     ##................................................Evaluate Solution.....................................................
     dqn = FINDER()
-    data_test_path = '../data/real/'
-#     data_test_name = ['Crime', 'HI-II-14', 'Digg', 'Enron', 'Gnutella31', 'Epinions', 'Facebook', 'Youtube', 'Flickr']
-    data_test_name = ['Crime','HI-II-14']
-    save_dir = '../results/FINDER_CN/real/StepRatio_%.4f/'%STEPRATIO
+    data_test_path = '/content/FINDER/code/FINDER_CN/data/real/'
+    #     data_test_name = ['Crime', 'HI-II-14', 'Digg', 'Enron', 'Gnutella31', 'Epinions', 'Facebook', 'Youtube', 'Flickr']
+    data_test_name = [f for f in listdir(data_test_path) if isfile(join(data_test_path, f))]
+    save_dir = '/content/FINDER/code/results/StepRatio_%.4f/'%STEPRATIO
     ## begin computing...
     df = pd.DataFrame(np.arange(2 * len(data_test_name)).reshape((2, len(data_test_name))), index=['solution', 'time'], columns=data_test_name)
     for i in range(len(data_test_name)):
         print('\nEvaluating dataset %s' % data_test_name[i])
-        data_test = data_test_path + data_test_name[i] + '.txt'
-        solution = save_dir + data_test_name[i] + '.txt'
+        data_test = data_test_path + data_test_name[i] 
+        solution = save_dir + data_test_name[i]
         t1 = time.time()
         # strategyID: 0:no insert; 1:count; 2:rank; 3:multiply
         ################################## modify to choose which strategy to evaluate
@@ -61,18 +74,39 @@ def EvaluateSolution(STEPRATIO, MODEL_FILE_CKPT, STRTEGYID):
         t2 = time.time()
         df.iloc[0, i] = score
         df.iloc[1, i] = t2 - t1
-        result_file = save_dir + '/MaxCCList_Strategy_' + data_test_name[i] + '.txt'
+        result_file = save_dir + '/MaxCCList_Strategy_' + data_test_name[i]
         with open(result_file, 'w') as f_out:
             for j in range(len(MaxCCList)):
                 f_out.write('%.8f\n' % MaxCCList[j])
         print('Data: %s, score:%.6f' % (data_test_name[i], score))
     df.to_csv(save_dir + '/solution_score.csv', encoding='utf-8', index=False)
 
+def findModel():
+    NUM_MIN, NUM_MAX = 30, 50
+    g_type = 'barabasi_albert'
+    VCFile = '/content/FINDER/code/FINDER_CN/models/%s/ModelVC_%d_%d.csv'%(g_type, NUM_MIN, NUM_MAX)
+    vc_list = []
+    with open(VCFile, newline='') as csvfile:
+         #reader = csv.DictReader(csvfile)
+         reader = csv.reader(csvfile, delimiter=',')
+         for i, row in enumerate(reader):
+            #print(row['first_name'], row['last_name'])
+            vc_list.append(float(row[0]))
+    start_loc = 0
+    plt.plot(vc_list)
+    plt.show()
+    min_vc = start_loc + np.argmin(vc_list[start_loc:])
+    best_model_iter = 500 * min_vc
+    best_model = '/content/FINDER/code/FINDER_CN/models/%s/nrange_%d_%d_iter_%d.ckpt' % (g_type, NUM_MIN, NUM_MAX, best_model_iter)
+    return best_model
+
 
 def main():
-    model_file_ckpt = 'nrange_30_50_iter_93300.ckpt'
+    model_file_ckpt = findModel()
+    print(model_file_ckpt)
     GetSolution(0.01, model_file_ckpt)
     EvaluateSolution(0.01, model_file_ckpt, 0)
+
 
 
 if __name__=="__main__":
